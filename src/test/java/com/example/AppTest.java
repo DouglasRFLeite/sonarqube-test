4fc8e45fc1717ecd28f88c924293cfeeab89831a
Douglas Leite <douglas.rochedo@gmail.com>
2025-07-08 15:14:01 -0300

    Add add method and test

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -8,4 +8,9 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void appCanAdd() {
+        App classUnderTest = new App();
+        assertEquals(3, classUnderTest.add(1, 2), "app should be able to add two numbers");
+    }
 }
